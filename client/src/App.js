1cb6c7d19e821c69960a672dc892cfa75a692f4a
Nathaniel Patterson <nathan@Nathaniels-iMac.local>
2020-12-03 13:25:29 -0500

    woop

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Web 36 rocks</header>
+    </div>
+  );
+}
+
+export default App;
