d0a3e8e4ba0ebfc3cd9958a51896c98c50945acc
Nathaniel Patterson <nathan@Nathaniels-iMac.local>
2020-12-03 12:55:24 -0500

    api working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 console.log("web 36 rocks");
 console.log(__dirname);
@@ -9,7 +10,7 @@ console.log(process.env.USER);
 console.log(process.env.PORT);
 
 const app = express();
-
+app.use(cors());
 app.use("/api/*", (_, res) => {
   res.status(200).json({ message: "Its Working" });
 });
