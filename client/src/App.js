55e1e1ef3d5f064b542e8989745980e77a9590a1
Nathaniel Patterson <nathan@Nathaniels-iMac.local>
2020-12-03 13:55:10 -0500

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,23 @@ import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+function url(path) {
+  return process.env.NODE_ENV === "development"
+    ? `http://localhost:5000${path}`
+    : path;
+}
 function App() {
-  const [working, setWorkgin] = useState([]);
-
+  const [data, setData] = useState("");
+  useEffect(() => {
+    // https://heroku.com/nice-app
+    // http://localhost:5000
+    fetch(url("/api/data"))
+      .then((res) => res.json())
+      .then((goods) => setData(goods.data));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">Web 36 rocks</header>
+      <header className="App-header">{data}</header>
     </div>
   );
 }
